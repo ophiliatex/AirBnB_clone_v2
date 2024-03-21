cbb170256b1d87eba8a77dacac7a277ac9ef58e2
ophiliatex <ophiliatex@gmail.com>
2024-03-20 21:27:13 -0700

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
 -- Prepares a MYSQL server for project.
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-
-USE hbnb_dev_db;
-
-GRANT ALL ON hbnb_dev_db.* TO hbnb_dev@localhost;
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
